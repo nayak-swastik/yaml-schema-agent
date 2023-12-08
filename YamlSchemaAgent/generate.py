17d5c55d4bcc75c5578e5fbe6d6ffe36e05a902c
swastik.nayak <swastik.nayak.eu@gmail.com>
2023-12-08 01:16:13 +0100

    MAJOR: Added multiple features that are required to validate yaml and generate schema. Logging is enabled. Report generation is now possible.

--- YamlSchemaAgent/generate.py
+++ YamlSchemaAgent/generate.py
@@ -1,10 +1,12 @@
 import json
+from logging import Logger
 
 from YamlSchemaAgent import schema_types
 
+logger_obj: Logger = None
 
-def decode_base_types(v):
-    print(f'add_{type(v).__name__.lower()}')
+def decode_base_types(k, v):
+    logger_obj.debug(f'{k} : {v} - decoded type: {type(v).__name__.lower()}')
     if hasattr(schema_types, f'add_{type(v).__name__.lower()}'):
         return getattr(schema_types, f'add_{type(v).__name__.lower()}')()
 
@@ -21,10 +23,12 @@ def walk_and_decode(data, jsonschema):
             else:
                 jsonschema[k] = v
         else:
-            jsonschema[k] = decode_base_types(v)
+            jsonschema[k] = decode_base_types(k, v)
 
 
-def run(yaml_context, schema_loc, output=None):
+def run(yaml_context, schema_loc, output=None, logger=None):
+    global logger_obj
+    logger_obj = logger
     schema = yaml_context.safe_load(open(schema_loc).read())
     json_schema = {
         "type": "object",
@@ -32,8 +36,10 @@ def run(yaml_context, schema_loc, output=None):
         },
     }
     walk_and_decode(schema, json_schema['properties'])
-    print("***"*20 + "GENERATED JSON SCHEMA" + "***"*20 )
-    print(json.dumps(json_schema, indent=2))
-    with open(output or 'ysg_'+ schema_loc.split('/')[-1], 'w') as gen_schema:
+    msg = "\n" + "***" * 10 + " GENERATED JSON SCHEMA " + "***" * 10
+    logger.debug(msg + '\n' + json.dumps(json_schema, indent=2))
+    output = output or 'ysg_' + schema_loc.split('/')[-1]
+    logger.info(f'Writing generated schema to: {output}')
+    with open(output, 'w') as gen_schema:
         gen_schema.write(json.dumps(json_schema, indent=4))
     return json_schema
